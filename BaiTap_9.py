e23a59aea4e5f27dd70daa29e4f7756272dbe6e3
DTH235663-Huynh-Cong-Huy <huy_dth235663@student.agu.edu.vn>
2025-11-30 23:02:45 +0700

    Delete BaiTap_9.py

--- BaiTap_9.py
+++
@@ -1,9 +0,0 @@
-# Nhập chiều dài và chiều rộng của hình chữ nhật
-length = int(input("Nhập chiều dài của hình chữ nhật: "))
-width = int(input("Nhập chiều rộng của hình chữ nhật: "))
-
-# Sử dụng vòng lặp để in hình chữ nhật
-for i in range(width):
-    for j in range(length):
-        print("*", end=" ")  # In dấu * với khoảng cách
-    print()  # Chuyển dòng sau khi in xong một hàng
